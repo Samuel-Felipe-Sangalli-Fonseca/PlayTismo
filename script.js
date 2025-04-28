12c4df8aed5d959b6c1b8639db5c4deab428e237
Samuel-Felipe-Sangalli-Fonseca <160272373+Samuel-Felipe-Sangalli-Fonseca@users.noreply.github.com>
2025-04-27 22:10:39 -0400

    .

--- script.js
+++
@@ -1,9 +0,0 @@
-var dragItem = document.querySelector('.dragElement');
-var dropZoneSet = Array.from(document.querySelectorAll('.dropZone'));
-
-dropZoneSet.forEach(dropzone => {
-    dropzone.addEventListener('dragover', (e) => {
-      e.preventDefault();
-      dropzone.appendChild(dragItem);
-  });
-  });
